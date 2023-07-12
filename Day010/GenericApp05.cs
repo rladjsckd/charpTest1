587cd78de5aa932bdecf9bf58a95e24ee945bd0a
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-07-12 17:42:24 +0900

    Create GenericApp05.cs

---
+++ Day010/GenericApp05.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApp05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            
+            queue.Enqueue("A");
+            queue.Enqueue("B");
+            queue.Enqueue("C");
+            queue.Enqueue("D");
+
+            while (queue.Count > 0) 
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
+    }
+}

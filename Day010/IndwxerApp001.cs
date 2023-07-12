ade855f624bd6d2c9e751005422e1c1472401337
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-07-12 17:40:34 +0900

    Create IndwxerApp001.cs

---
+++ Day010/IndwxerApp001.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace IndwxerApp001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 123, 456, 789 };
+            ArrayList list = new ArrayList(arr);
+
+            ArrayList list2 = new ArrayList();
+            list2.Add(1);
+            list2.Add(2);
+
+            foreach (int i in list2) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

8baa4274d9bdd49a0d0084b02453ba3b494bc231
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-07-14 16:06:16 +0900

    Create Quiz09.cs

---
+++ Day012/Quiz09.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            switch (n) 
+            {
+                case 1:
+                    Console.WriteLine("enjoy");
+                    break;
+                case 2:
+                    Console.WriteLine("enjoy");
+                    break;
+                case 3:
+                    Console.WriteLine("enjoy");
+                    break;
+                case 4:
+                    Console.WriteLine("oops");
+                    break;
+                case 5:
+                    Console.WriteLine("enjoy");
+                    break;
+                case 6:
+                    Console.WriteLine("enjoy");
+                    break;
+                case 7:
+                    Console.WriteLine("enjoy");
+                    break;
+            }
+        }
+    }
+}

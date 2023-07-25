99cd60af0a043c0a35f6102219bea680087deb3b
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-07-25 17:03:40 +0900

    Create MemoryStreamQuiz.cs

---
+++ Day016/MemoryStreamQuiz.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryStreamQuiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = "C:\\Users\\Admin\\abc.txt";
+            //StreamReader sr = new StreamReader(path, Encoding.UTF8);
+            //string txt = sr.ReadToEnd();
+            //sr.Close();
+
+            //Console.WriteLine(txt);
+
+            //MemoryStream ms = new MemoryStream();
+            //byte[] strBytes = Encoding.UTF8.GetBytes(txt);
+            //ms.Write(strBytes, 0, strBytes.Length);
+
+            //ms.Position = 0;
+            //StreamReader sr2 = new StreamReader(ms, Encoding.UTF8, true);
+            //string txt2 = sr2.ReadToEnd();
+            //Console.WriteLine(txt2);
+
+
+        }
+    }
+}

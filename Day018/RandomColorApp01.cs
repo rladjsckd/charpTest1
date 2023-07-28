9b884ed5e778c31002cec5695ff0ec0bd5199c18
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-07-28 17:26:35 +0900

    Create RandomColorApp01.cs

---
+++ Day018/RandomColorApp01.cs
@@ -0,0 +1,29 @@
+namespace RandomColorApp01
+{
+    public partial class Form1 : Form
+    {
+        private Random rand;
+        public Form1()
+        {
+            InitializeComponent();
+
+            Load += Form1_Load;
+
+            MouseClick += RandomColorAction;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rand = new Random();
+        }
+
+        //사용자 정의 메소드
+        private void RandomColorAction(object sender, MouseEventArgs e) 
+        {
+            int red = rand.Next(0, 256);
+            int green = rand.Next(0, 256);
+            int blue = rand.Next(0, 256);
+            this.BackColor = Color.FromArgb(red, green, blue);
+        }
+    }
+}

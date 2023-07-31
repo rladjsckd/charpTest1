0ea91ee5c6f06a9b36680d2546980d4261594143
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-07-31 17:22:11 +0900

    Create FormStyle02.cs

---
+++ Day019/FormStyle02.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormStyle02
+{
+    class Program : Form
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Application.Run(p);
+        }
+    }
+}

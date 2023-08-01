10318e2174a8821b2d6f14351d9d384b6c2f8bbb
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-08-01 17:37:03 +0900

    Create WindowsFormsApp3Win.cs

---
+++ Day020/WindowsFormsApp3Win.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private int cnt = 0;
+        private Thread t;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            t = new Thread(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Invoke((Action)(() => label1.Text = cnt++ + ""));
+                        Thread.Sleep(1000);
+                    }
+                    catch (Exception ex)
+                    {
+                        break;
+                    }
+
+                }
+            });
+            t.Start();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t.Interrupt();
+        }
+    }
+}

9e2b1a3f3736aa2b7c2ea48898c0870541585e75
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-08-02 17:25:35 +0900

    Create WindowsFormsApp4.cs

---
+++ Day021/WindowsFormsApp4.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\Admin\Desktop.jpg";
+            pictureBox1.Load("파일path");
+            pictureBox1.Image = Image.FromFile(path);
+        }
+    }
+}

4e93fd08d05d2da2ae35cf551ec5776a33adcfe4
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-08-16 16:58:40 +0900

    Create html02.cs

---
+++ Day022/html02.cs
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ko"> 
+<head>
+   <meta charset="UTF-8">
+   <meta name ="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   <h2>동물원</h2>
+      <u1>
+         <li>호랑이</li>
+         <li>늑대</li>
+         <li>원숭이</li>
+      </u1>
+   <h2>식물원</h2>
+      <u1>
+         <li>장미</li>
+         <li>소나무</li>
+         <li>전나무</li>
+         <li>개나리</li>
+      </u1>
+</body>
+</html>

3d9f0e0aa3c5ea50d2bdce6bb3743f645e93cbc9
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-08-16 17:00:38 +0900

    Create WebApp003.cs

---
+++ Day022/WebApp003.cs
@@ -0,0 +1,12 @@
+@page
+@model IndexModel
+@{
+    ViewData["Title"] = "First my webApp~~!!";
+}
+
+<div>
+    <h1> 환영합니다~!!</h1>
+    <p>ASP.NET Core를 공부해봅시다.</p>
+    <p><h2>ASP.NET 프로젝트를 만들었습니다.</h2></p>
+
+</div>

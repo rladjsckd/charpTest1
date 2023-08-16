89f24ee81fb3b62a3d8b4de68d4b746acc5eed95
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-08-16 16:59:16 +0900

    Create html01.cs

---
+++ Day022/html01.cs
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+   <meta charset="UTF-8">
+   <title>Document</title>>
+</head>
+<body>
+
+<h2>튤립</h2>
+
+안녕하세요. <br>반갑습니다. <p>
+<b>튤립</b>은 여러 <i>물품</i>으로써
+
+<br>
+<b><i>나의 튤립</i></b>
+</body>
+</html>

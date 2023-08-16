ca1b6fdcf24f091183d94a7adc1cbad87b403f69
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-08-16 16:59:30 +0900

    Create html.cs

---
+++ Day022/html.cs
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ko"> 
+<head>
+   <meta charset="UTF-8">
+   <meta name ="viewport" content="width=device-width, initial-scale=1.0">
+   <title>웹 페이지 제목</title>>
+<title>Welcome To My Home</title>
+</head>
+<body>
+
+   <h1><a href="https://www.naver.com" target="_blank">네이버</a></h1>
+
+</body>
+</html>

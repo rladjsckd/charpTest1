c367c0babc5851a42a3405bc8f3fd59def653878
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-08-17 17:39:53 +0900

    Create ex5-4.cs

---
+++ Day023/ex5-4.cs
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="../../common/mystyle.css">
+</head>
+<body>
+    <h1>제목입니다.</h1>
+    <p>
+        주소
+        경상북도 안동시 경동로 1375
+        전화
+        054-820-5114
+        입학
+        2024학년도 입학안내
+        전공
+        설치학과 및 선발 정보 보기 메가스터디 제공
+    </p>
+</body>
+</html>

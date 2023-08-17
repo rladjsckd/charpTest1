9375485593c2116496e1ade59c2af5c30e315c82
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-08-17 17:39:27 +0900

    Create ex4-4.cs

---
+++ Day023/ex4-4.cs
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+<style>
+    table, tr, th, td{
+            border: solid black 2px;
+            border-collapse: collapse;
+            padding: 4px;
+        }
+</style>
+</head>
+<body>
+    <table>
+        <tr>
+            <th colspan="8">2023KBO리그 팀순위</th>
+        </tr>
+        <tr>
+            <th>순위</th>
+            <th>팀명</th>
+            <th>경기</th>
+            <th>승</th>
+            <th>무</th>
+            <th>패</th>
+            <th>승률</th>
+            <th>게임차</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td><img src="../img/LG.jpg"> LG</td>
+            <td>100</td>
+            <td>62</td>
+            <td>2</td>
+            <td>36</td>
+            <td>0.633</td>
+            <td>0.0</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td><img src="../img/SSG.jpg">SSG</td>
+            <td>99</td>
+            <td>55</td>
+            <td>43</td>
+            <td>1</td>
+            <td>0.561</td>
+            <td>7.0</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td><img src="../img/KT.jpg">KT</td>
+            <td>102</td>
+            <td>55</td>
+            <td>45</td>
+            <td>2</td>
+            <td>0.550</td>
+            <td>8.0</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td><img src="../img/NG.jpg">NC</td>
+            <td>99</td>
+            <td>50</td>
+            <td>47</td>
+            <td>2</td>
+            <td>0.515</td>
+            <td>11.5</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td><img src="../img/두산.jpg">두산</td>
+            <td>99</td>
+            <td>49</td>
+            <td>49</td>
+            <td>1</td>
+            <td>0.5</td>
+            <td>13.0</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td><img src="../img/KIA.jpg">KIA</td>
+            <td>95</td>
+            <td>46</td>
+            <td>47</td>
+            <td>2</td>
+            <td>0.495</td>
+            <td>13.5</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td><img src="../img/롯데.jpg">롯데</td>
+            <td>100</td>
+            <td>49</td>
+            <td>51</td>
+            <td>0</td>
+            <td>0.490</td>
+            <td>14</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td><img src="../img/한화.jpg">한화</td>
+            <td>99</td>
+            <td>41</td>
+            <td>52</td>
+            <td>6</td>
+            <td>0.441</td>
+            <td>18.5</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td><img src="../img/삼성.jpg">삼성</td>
+            <td>102</td>
+            <td>42</td>
+            <td>59</td>
+            <td>1</td>
+            <td>0.416</td>
+            <td>21.5</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td><img src="../img/키움.jpg">키움</td>
+            <td>109</td>
+            <td>43</td>
+            <td>63</td>
+            <td>3</td>
+            <td>0.406</td>
+            <td>23.0</td>
+        </tr>
+    </table>
+</body>
+</html>

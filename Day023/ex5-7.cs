65e3e948b014909c06ff745f59b5a93bfcf5a930
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-08-17 17:40:08 +0900

    Create ex5-7.cs

---
+++ Day023/ex5-7.cs
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        h1{
+            color:#00f;
+            text-shadow: 3px 3px 5px #444444;
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <h1>페퍼민트 허브</h1>
+</body>
+</html>

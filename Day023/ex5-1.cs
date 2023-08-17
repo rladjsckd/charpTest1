8fc9e1ab72925b3505b5608722cbfa6361d36802
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-08-17 17:39:40 +0900

    Create ex5-1.cs

---
+++ Day023/ex5-1.cs
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<style>
+    h1{
+        color :red;
+        font-size: 30px;
+    }
+    h2{
+        color :blue;
+    }
+    h3{
+        color :yellow;
+    }
+</style>
+</head>
+<body>
+    <h1 style="color: red; font-size:30px">제목입니다.</h1>
+    <h2> 제목 2입니다.</h2>
+    <h3> 제목 3입니다.</h3>
+</body>
+</html>

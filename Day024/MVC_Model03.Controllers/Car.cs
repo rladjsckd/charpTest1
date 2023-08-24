3992c02c979c4bd98d6b3b20a7d833ba5fcc6df6
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-08-24 17:10:29 +0900

    Create Car.cs

---
+++ Day024/MVC_Model03.Controllers/Car.cs
@@ -0,0 +1,9 @@
+namespace MVC_Model03.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string Brand { get; set; }
+        public int Speed { get; set; }    
+    }
+}

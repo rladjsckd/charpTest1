1f07a00f9b4edf30a06a273c6c50eb0c8defb531
rladjsckd <137870297+rladjsckd@users.noreply.github.com>
2023-08-30 17:22:11 +0900

    Create HomeController.cs

---
+++ Day026/WebAppReport2/HomeController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebAppReport02.Models;
+
+namespace WebAppReport02.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Chart()
+        {
+            return View();
+        }
+
+        public IActionResult Test1()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
